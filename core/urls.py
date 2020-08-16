dae630990417171e4b1eab9bc94155c466c8486f
Josphat Gitogo <josphat.gitogo@gmail.com>
2020-08-16 17:28:35 +0300

    Initial Commit

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+"""
+License: MIT
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.contrib import admin
+from django.urls import path, include  # add this
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('processdata.urls')),
+    path('', include('app.urls')),  # add this
+]
